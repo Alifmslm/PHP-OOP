ace27e3c7ac72fc3622132a4e04c10f1ba7239f0
Alifmslm <Alifmslm01@gmail.com>
2023-01-24 11:02:52 +0700

    mandatory-parameter

--- class.php
+++ class.php
@@ -11,20 +11,33 @@ class mobil2 {
     }
 }
 
+//bisa kita anggap seperti template
 class mobil3 {
     //class dengan property
     public $pintu;
     public $roda;
 
     public function jalan() {
-        echo "Mobil Berjalan";
+        echo "Mobil Berjalan" . PHP_EOL;
     }
 }
 
+//harus diinisialisasi seperti dibawah jika ingin menampilkan hasil/value-nya
 $avanza = new Mobil3();
 $avanza->roda = 4;
 
 echo $avanza->jalan();
+
+class Koneksi {
+    public function connect
+        ($username, $password, $host = 'localhost', $port = 3306) {
+            echo "$username, $password, $host, $port" . PHP_EOL;
+    }
+}
+
+$database = new Koneksi();
+$database->connect('root', '');
+
 
 
 ?>
