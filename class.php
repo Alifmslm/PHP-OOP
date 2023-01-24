9aa3f4d84cb47d004b76ce31e37212f44c55edb9
Alifmslm <Alifmslm01@gmail.com>
2023-01-24 11:33:11 +0700

    coba-coba

--- class.php
+++ class.php
@@ -15,10 +15,10 @@ class mobil2 {
 class mobil3 {
     //class dengan property
     public $pintu;
-    public $roda;
+    public $roda = 4;
 
     public function jalan() {
-        echo "Mobil Berjalan" . PHP_EOL;
+        return "Mobil Berjalan" . PHP_EOL;
     }
 }
 
@@ -30,6 +30,9 @@ echo $avanza->jalan();
 
 class Koneksi {
     public function connect
+        //variable username & password adalah mandatory parameter
+        //mandatory parameter adalah parameter yg tidak memiliki default value
+        //mandatory harus diberikan value saat dieksekusi
         ($username, $password, $host = 'localhost', $port = 3306) {
             echo "$username, $password, $host, $port" . PHP_EOL;
     }
