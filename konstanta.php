4f62e8c856407c9588e0b0a95830594a0db96dcc
Alifmslm <Alifmslm01@gmail.com>
2023-01-25 14:36:34 +0700

    konstanta OOP

---
+++ konstanta.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Lingkaran {
+    public const PI = 3.14;
+    public static function luas($jari) {
+        echo self::PI * $jari * $jari;
+    }
+}
+
+//cara lama dipakai jika si property/method tidak ad keyword static
+$lingkaran = new lingkaran();
+echo Lingkaran::PI;
+echo PHP_EOL;
+echo $lingkaran->luas(7);
+echo PHP_EOL;
+
+
+//contoh static
+echo Lingkaran::PI;
+echo PHP_EOL;
+echo Lingkaran::luas(7);
+echo PHP_EOL;
+
